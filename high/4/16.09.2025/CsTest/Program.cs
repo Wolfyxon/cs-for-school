5793fc1a702b5448d5ee25909feb8731aff810c7
Wolfyxon <wolfyxon@gmail.com>
2025-09-16 09:15:09 +0200

    init

---
+++ high/4/16.09.2025/CsTest/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Podaj imię: ");
+string? name = Console.ReadLine();
+
+if(name != null)
+{
+    Console.WriteLine($"Witaj, {name}!");
+} else
+{
+    Console.WriteLine("Nie podano imienia");
+}
+
+Console.Write("Podaj A: ");
+float a = float.Parse(Console.ReadLine()!);
+
+Console.Write("Podaj B: ");
+float b = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Wynik: {a + b}");
+
