41f94ee094fc10cfa3a8348405bc865942c11f62
Wolfyxon <wolfyxon@gmail.com>
2025-11-18 09:17:35 +0100

    Another

---
+++ high/4/18.11.2025/zestaw/zestaw/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.Write("Podaj liczbę: ");
+        int num = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine($"Silnia {num}!: {Factorial(num)}");
+    }
+
+    static int Factorial(int n)
+    {
+        int res = 1;
+
+        for(int i = 1; i <= n; i++)
+        {
+            res *= i;
+        }
+
+        return res;
+    }
+}
