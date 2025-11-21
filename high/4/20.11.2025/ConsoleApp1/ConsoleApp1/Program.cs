e5e8abc21fd650041c5db516c289d6f2539bb28e
Wolfyxon <wolfyxon@gmail.com>
2025-11-21 11:32:55 +0100

    Init 20.11.2025

---
+++ high/4/20.11.2025/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿class Program
+{
+    public static void Main(String[] args)
+    {
+
+    }
+}
+
+class CharLib
+{
+    const String VOWELS = "aąeęiouóyAĄEĘIOUÓY";
+
+    public static int CountVowels(String? text)
+    {
+        int res = 0;
+
+        if(text != null)
+        {
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (IsVowel(text[i])) {
+                    res += 1;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static String RemoveDuplicateChars(String? text)
+    {
+        String res = "";
+
+        if(text != null)
+        {
+            for(int i = 0; i < text.Length; i++)
+            {
+                Char currentChar = text[i];
+
+                if (i == 0 || res[i - 1] != currentChar)
+                {
+                    res += currentChar;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static bool IsVowel(Char ch)
+    {
+        for(int i = 0; i < VOWELS.Length; i++)
+        {
+            if (VOWELS[i] == ch)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
