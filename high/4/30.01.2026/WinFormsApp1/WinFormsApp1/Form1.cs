0508965873a00d7432d8fe84694f9ac15a6d9ea4
Wolfyxon <wolfyxon@gmail.com>
2026-01-30 12:05:03 +0100

    Implemented logic

--- high/4/30.01.2026/WinFormsApp1/WinFormsApp1/Form1.cs
+++ high/4/30.01.2026/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -5,11 +5,38 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+
+            RegisterSlider(sliderR);
+            RegisterSlider(sliderG);
+            RegisterSlider(sliderB);
+        }
+
+        void RegisterSlider(TrackBar slider)
+        {
+            slider.ValueChanged += new EventHandler((object? sender, EventArgs e) => UpdateColor());
+        }
+
+        void UpdateColor()
+        {
+            int r = sliderR.Value;
+            int g = sliderG.Value;
+            int b = sliderB.Value;
+
+            valueR.Text = r.ToString();
+            valueG.Text = g.ToString();
+            valueB.Text = b.ToString();
+
+            colorPanel.BackColor = Color.FromArgb(r, g, b);
         }
 
         private void btnGet_Click(object sender, EventArgs e)
         {
+            int r = sliderR.Value;
+            int g = sliderG.Value;
+            int b = sliderB.Value;
 
+            colorText.Text = $"{r}, {g}, {b}";
+            colorText.BackColor = Color.FromArgb(r, g, b);
         }
     }
 }
