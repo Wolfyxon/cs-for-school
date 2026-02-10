d21c8f3d35bb4505bbe40a9a945eca6aed7ab9a8
Wolfyxon <wolfyxon@gmail.com>
2026-02-10 09:30:19 +0100

    10.02.2026, this task is crappy as heck

---
+++ high/4/10.02.2026/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        Film film = new Film("Pięć Koszmarnych Nocy");
+
+        film.SetTytul("Pięć Koszmarnych Nocy 2");
+        Console.WriteLine($"tytuł: {film.GetTytul()} wypożyczenia: {film.GetWypozyczenia()}");
+        
+        Console.Write($"Przed inkrementacją: {film.GetWypozyczenia()} ");
+        film.Inkrementuj();
+        Console.WriteLine($"po inkrementacji: {film.GetWypozyczenia()}");
+    }
+}
+
+class Film
+{
+    private String _tytul;
+    private int _wypozyczenia;
+
+    public Film(String tytul)
+    {
+        this._tytul = tytul;
+        this._wypozyczenia = 0;
+    }
+
+    public void Inkrementuj()
+    {
+        this._wypozyczenia += 1;
+    }
+
+    public void SetTytul(String tytul)
+    {
+        this._tytul = tytul;
+    }
+
+    public string GetTytul()
+    {
+        return this._tytul;
+    }
+
+    public int GetWypozyczenia()
+    {
+        return this._wypozyczenia;
+    }
+}
